6aa6883361acb26e9a0e10a75be05e6abfbd832d
Masha-Menaga <rameast.mass@gmail.com>
2024-09-12 16:05:22 +0530

    Final SpringBoot File including 2 APIs with DB Connectivity

---
+++ EcommerceProducts/src/main/java/com/ecommerce/entity/Category.java
@@ -0,0 +1,44 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long category_id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+   
+    // Constructors, Getters, and Setters
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return category_id;
+    }
+
+    public void setId(Long category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
